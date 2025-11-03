368670795975b5ec9b4a13782eede490a1a35d22
Myself <38400384+Myself-Bloons@users.noreply.github.com>
2025-11-03 15:44:28 +0100

    Add files via upload

---
+++ ModHelperData.cs
@@ -0,0 +1,14 @@
+namespace AllRaceMinigames;
+
+public static class ModHelperData
+{
+    public const string Name = "All Race Minigames";
+    public const string Description = "Convert Rogue Legends tiles into timed race minigames. Convert just minigames or ALL tiles - fully configurable! (ISAB dream)";
+    public const string Version = "1.0.0";
+    public const string RepoOwner = "Myself-Bloons";
+    public const string RepoName = "All-Race-Minigames";
+    public const string Author = "Myself";
+    public const string WorksOnVersion = "51.2";
+    public const string DllName = "AllRaceMinigames.dll";
+    public const string Icon = "Icon.png";
+}
